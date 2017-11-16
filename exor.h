79465989eb62be67d3926042d568c7d886610823
PauliusMorku <paulius.morku@gmail.com>
2017-11-17 00:58:00 +0100

    Almost finished second exercise

--- exor.h
+++ exor.h
@@ -5,5 +5,34 @@
 #include "nand.h"
 
 // TODO: Insert your code here
+SC_MODULE(exor)
+{
+    sc_out<bool> Z;
+    sc_in<bool> A, B;
+
+    nand nanda, nandb, nandc, nandd;
+
+    sc_signal<bool> tempa, tempb, tempc;
+
+    SC_CTOR(exor): A("A"), B("B"), Z("Z"), nanda("nanda"), nandb("nandb"), nandc("nandc"), nandd("nandd")
+    {
+        nanda.A.bind(A);
+        nanda.B.bind(B);
+        nanda.Z.bind(tempa);
+
+        nandb.A.bind(tempa);
+        nandb.B.bind(B);
+        nandb.Z.bind(tempb);
+
+        nandc.A.bind(A);
+        nandc.B.bind(tempa);
+        nandc.Z.bind(tempc);
+
+        nandd.A.bind(tempa);
+        nandd.B.bind(tempb);
+        nandd.Z.bind(Z);
+
+    }
+};
 
 #endif // EXOR_H
--- nand.h
+++ nand.h
@@ -4,5 +4,21 @@
 #include "systemc.h"
 
 // TODO: Insert your code here
+SC_MODULE(nand)
+{
+    sc_out<bool> Z;
+    sc_in<bool> A, B;
+
+    void do_nand() {
+        Z.write(!(A.read() & B.read()));
+    }
+
+    SC_CTOR(nand): A("A"), B("B"), Z("Z")
+    {
+        SC_METHOD(do_nand);
+        sensitive << A << B;
+    }
+};
+
 
 #endif
